37b28dc33d31e9ec7511be48f740928bb263c7d5
Ahmed Rangel <ahmedrangel@outlook.com>
2025-08-06 20:05:43 -0500

    fix: add missing order enum

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ export { searchAnimesByFilter } from "./utils/scrapers/searchAnimesByFilter";
 export { searchAnimesByURL } from "./utils/scrapers/searchAnimesByUrl";
 export { getLatest } from "./utils/scrapers/getLatest";
 export { getOnAir } from "./utils/scrapers/getOnAir";
-export { AnimeGenreEnum as GenreEnum, AnimeTypeEnum as TypeEnum, AnimeStatusEnum as StatusEnum } from "./utils/helpers";
+export { AnimeGenreEnum as GenreEnum, AnimeTypeEnum as TypeEnum, AnimeStatusEnum as StatusEnum, FilterOrderEnum as OrderEnum } from "./utils/helpers";
