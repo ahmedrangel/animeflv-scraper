47b0c44efb4b0343ed1d9926a3a13e99be163c4f
Ahmed Rangel <ahmedrangel@outlook.com>
2026-08-12 14:33:44 -0500

    build!: drop cjs, migrate from unbuild to obuild, update deps

--- build.config.ts
+++ build.config.ts
@@ -1,11 +1,7 @@
-import { defineBuildConfig } from "unbuild";
+import { defineBuildConfig } from "obuild/config";
 
 export default defineBuildConfig({
-  declaration: true,
   entries: [
     "./src/index"
-  ],
-  rollup: {
-    emitCJS: true
-  }
+  ]
 });
